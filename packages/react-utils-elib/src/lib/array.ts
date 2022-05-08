99ba779f81b0dfcd8b8cb093cc380c338a40361e
endalk200 <eb808826@gmail.com>
2022-05-08 13:16:16 +0300

    chore(utils): added new tests and refactored array utility functions

--- packages/react-utils-elib/src/lib/array.ts
+++ packages/react-utils-elib/src/lib/array.ts
@@ -4,86 +4,86 @@
  * @param array
  * @returns Returns the first element of an array or undefined if the array is empty.
  */
-export function getFirstItem<T>(array: T[]): T | undefined {
+export const getFirstItem = <T>(array: T[]): T | undefined => {
 	return array != null && array.length ? array[0] : undefined;
-}
+};
 
 /**
  *
  * @param array Returns the last element of an array.
  * @returns Returns the last element of an array or undefined if the array is empty.
  */
-export function getLastItem<T>(array: T[]): T | undefined {
+export const getLastItem = <T>(array: T[]): T | undefined => {
 	const length = array == null ? 0 : array.length;
 	return length ? array[length - 1] : undefined;
-}
+};
 
 /**
  * Returns the previous item in an array.
  *
- * @param index {number} The index of the current item.
- * @param array {Array} The array to search.
- * @param loop {boolean} Whether to loop around the array.
+ * @param {number} index The index of the current item.
+ * @param {Array} array The array to search.
+ * @param {boolean} loop Whether to loop around the array.
  * @returns Returns the previous item in an array.
  */
-export function getPrevItem<T>(index: number, array: T[], loop = true): T {
+export const getPrevItem = <T>(index: number, array: T[], loop = true): T => {
 	const prevIndex = getPrevIndex(index, array.length, loop);
 	return array[prevIndex];
-}
+};
 
 /**
  *
- * @param index Returns the next item in an array.
- * @param array {Array} The array to search.
- * @param loop {boolean} Whether to loop around the array.
- * @returns {any} Returns the next item in an array.
+ * @param {number} index Returns the next item in an array.
+ * @param {Array} array The array to search.
+ * @param {boolean} loop Whether to loop around the array.
+ * @returns {T} Returns the next item in an array.
  */
-export function getNextItem<T>(index: number, array: T[], loop = true): T {
+export const getNextItem = <T>(index: number, array: T[], loop = true): T => {
 	const nextIndex = getNextIndex(index, array.length, 1, loop);
 	return array[nextIndex];
-}
+};
 
 /**
  * Removes an item from an array with the specified index.
  *
- * @param array {Array} The array to search.
- * @param index {number} The index of the item to remove.
+ * @param {Array} array The array to search.
+ * @param {number} index The index of the item to remove.
  * @returns {Array} Returns the array with the item removed.
  */
-export function removeIndex<T>(array: T[], index: number): T[] {
+export const removeIndex = <T>(array: T[], index: number): T[] => {
 	return array.filter((_, idx) => idx !== index);
-}
+};
 
 /**
  * Adds and item at the end of an array.
- * @param array
- * @param item {any} The item to add.
+ * @param {Array} array
+ * @param {unknown} item The item to add.
  * @returns {Array} Returns the new array.
  */
-export function addItem<T>(array: T[], item: T): T[] {
+export const addItem = <T>(array: T[], item: T): T[] => {
 	return [...array, item];
-}
+};
 
 /**
  * Removes an item from an array.
  *
- * @param array {Array} The array to search.
- * @param item {any} The item to remove.
+ * @param {Array} array The array to search.
+ * @param {unknown} item The item to remove.
  * @returns {Array} Returns the array with the item removed.
  */
-export function removeItem<T>(array: T[], item: T): T[] {
+export const removeItem = <T>(array: T[], item: T): T[] => {
 	return array.filter((eachItem) => eachItem !== item);
-}
+};
 
 /**
  * Get the next index based on the current index and step.
  *
- * @param currentIndex the current index
- * @param length the total length or count of items
- * @param step the number of steps
- * @param loop whether to circle back once `currentIndex` is at the start/end
+ * @param {number} currentIndex the current index
+ * @param {number} length the total length or count of items
+ * @param {number} step the number of steps
+ * @param {boolean} loop whether to circle back once `currentIndex` is at the start/end
  */
-export function getNextIndex(currentIndex: number, length: number, step = 1, loop = true): number {
+export const getNextIndex = (currentIndex: number, length: number, step = 1, loop = true): number => {
 	const lastIndex = length - 1;
 
 	if (currentIndex === -1) {
@@ -102,28 +102,28 @@ export function getNextIndex(currentIndex: number, length: number, step = 1, loo
 	}
 
 	return nextIndex;
-}
+};
 
 /**
  * Get's the previous index based on the current index.
  * Mostly used for keyboard navigation.
  *
- * @param index - the current index
- * @param count - the length or total count of items in the array
- * @param loop - whether we should circle back to the
+ * @param {number} index - the current index
+ * @param {number} count - the length or total count of items in the array
+ * @param {boolean} loop - whether we should circle back to the
  * first/last once `currentIndex` is at the start/end
  */
-export function getPrevIndex(index: number, count: number, loop = true): number {
+export const getPrevIndex = (index: number, count: number, loop = true): number => {
 	return getNextIndex(index, count, -1, loop);
-}
+};
 
 /**
  * Converts an array into smaller chunks or groups.
  *
- * @param array the array to chunk into group
- * @param size the length of each chunk
+ * @param {Array} array the array to chunk into group
+ * @param {number} size the length of each chunk
  */
-export function chunk<T>(array: T[], size: number): T[][] {
+export const chunk = <T>(array: T[], size: number): T[][] => {
 	return array.reduce((rows: T[][], currentValue: T, index: number) => {
 		if (index % size === 0) {
 			rows.push([currentValue]);
@@ -132,22 +132,22 @@ export function chunk<T>(array: T[], size: number): T[][] {
 		}
 		return rows;
 	}, [] as T[][]);
-}
+};
 
 /**
  * Gets the next item based on a search string
  *
- * @param items array of items
- * @param searchString the search string
- * @param itemToString resolves an item to string
+ * @param {Array} items array of items
+ * @param {String} searchString the search string
+ * @param {Function} itemToString resolves an item to string
  * @param currentItem the current selected item
  */
-export function getNextItemFromSearch<T>(
+export const getNextItemFromSearch = <T>(
 	items: T[],
 	searchString: string,
 	itemToString: (item: T) => string,
 	currentItem: T
-): T | undefined {
+): T | undefined | null => {
 	if (searchString == null) {
 		return currentItem;
 	}
@@ -183,6 +183,5 @@ export function getNextItemFromSearch<T>(
 		return items[nextIndex];
 	}
 
-	// a decent fallback to the currentItem
-	return currentItem;
-}
+	return null;
+};
